1554c5eaaf768157ea6fe645989c679cd54cc23b
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-25 19:40:38 +0100

    Esercizio mail completato e validato

--- mail/main.js
+++ mail/main.js
@@ -1,18 +1,32 @@
+//array
 const emailArray = ['laroccapierluigi1998@gmail.com', 'yakoiyeicafroi-2435@gmail.com', 'weilofrappipri-7097@gmail.com', 'togreugreyawau-5046@gmail.com','sazefrafrubi-6574@gmail.com'];
 
-let userEmail = prompt('Inserisci la tua e-mail?');
+//variabile button accedi
+let buttonAccedi = document.getElementById("button-addon2");
 
-let control = false;
-for(let i = 0; i < emailArray.length; i++) {
-    if (emailArray[i] == userEmail){
+
+buttonAccedi.addEventListener('click',
+    function(){
+        //acquisizione del dato inserito dall'utente
+        let userEmail = document.getElementById("user-email").value;
+        //inizializzazione della variabile per il controllo
+        let control = false;
+        //ciclo per la validazione dell'email
+        for(let i = 0; i < emailArray.length; i++) {
+            if (emailArray[i] == userEmail){
+                
+                control = true
+            }  
+        }
         
-        control = true
-    }  
-}
- 
-if(control == true){
-    alert('email presente');
-}
-else{
-    alert('Email non presente')
-}
+        //output successiva alla validazione
+        if(control == true){
+            const htmlConsentito = document.getElementById('accesso-consentito');
+            htmlConsentito.classList.remove('ms_none');
+        } else{
+            const htmlNonConsentito = document.getElementById('accesso-negato');
+            htmlNonConsentito.classList.remove('ms_none');
+        }
+
+    }
+);
