6fa91a9067eba20a6394f4cc9fa4aad0d87f64d1
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-01-25 20:35:13 +0100

    Esercizio dei dadi completato e validato

--- dadi/main.js
+++ dadi/main.js
@@ -1,13 +1,61 @@
-let dadoUtente = Math.floor(Math.random() * 6) + 1;
-console.log(dadoUtente);
+//variabile button 
+let buttonPlay = document.getElementById("play-game");
+let buttonReset = document.getElementById("reset-game");
 
-let dadoComputer = Math.floor(Math.random() * 6) + 1;
-console.log(dadoComputer);
+//evento del click su gioca
+buttonPlay.addEventListener('click',
+    function() {
+        //numero dell'utente
+        let dadoUtente = Math.floor(Math.random() * 6) + 1;
+        console.log(dadoUtente);
 
-if (dadoUtente > dadoComputer) {
-    console.log("utente vince")
-} else if (dadoUtente < dadoComputer){
-    console.log("computer vince")
-}else{
-    console.log("pareggio")
-}
+        //numero del computer
+        let dadoComputer = Math.floor(Math.random() * 6) + 1;
+        console.log(dadoComputer);
+        
+        //variabile dell'esito della partita inizializzata
+        let esitoPartita;
+
+        //ciclo per trovare l'esito della partita
+        if (dadoUtente > dadoComputer) {
+        //vittoria utente
+        esitoPartita = "Complimenti hai vinto !!!"
+        } else if (dadoUtente < dadoComputer){
+         //vittoria computer
+        esitoPartita = "Mi dispiace il computer ha vinto"
+        }else{
+        //pareggio
+        esitoPartita = "Avete pareggiato"
+    }
+
+    //stampa del numero dell'utente
+    document.getElementById('user-number').innerHTML = dadoUtente;
+
+    //stampa del numero del computer
+    document.getElementById('computer-number').innerHTML = dadoComputer;
+
+    //stampa del prezzo del biglietto
+    document.getElementById('esito').innerHTML = esitoPartita;
+    
+    //aggiunta della classe active per mostrare il risultato
+    const htmlEsitoPartita = document.getElementById('box-esito');
+    htmlEsitoPartita.classList.remove('ms_none');
+
+
+
+}    
+    
+);
+
+//evento del click su reset
+buttonReset.addEventListener('click',
+    function(){
+        //resetto i dati alla versione iniziale
+        document.getElementById('user-number').innerHTML = "";
+        document.getElementById('computer-number').innerHTML = "";
+        //riaggiungo la classe ms-none
+        const htmlEsitoPartita = document.getElementById('box-esito');
+        htmlEsitoPartita.classList.add('ms_none');
+    
+    }   
+);
